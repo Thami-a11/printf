42b6cf9705ad9a1e8cf0b5b936d14fd2e76cb85d
Thamsanqa <thamsanqadyantyi4@gmail.com>
2023-04-25 15:35:16 -0700

    intege added.

--- _putchar.c
+++ _putchar.c
@@ -10,5 +10,28 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (buffer(c));
+}
+
+/**
+ * buffer - Save c in Buffer
+ * @c: Character
+ *
+ * Return: 1
+ **/
+int buffer(char c)
+{
+	static char buffering[1024];
+	static int i;
+
+	if (c == -1 || i == 1024)
+	{
+		write(1, buffering, i);
+		i = 0;
+	}
+
+	if (c != -1)
+		buffering[i++] = c;
+
+	return (1);
 }
